5b2734a0add67c1f5f70288923cb5b2f7db422b4
Alejandro Miranda <amiranda110500@gmail.com>
2022-01-08 10:51:57 +0000

    Added user registration

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,13 @@
+import { SignUpForm } from "../components/SignUpForm";
 
 export const RegisterPage = () => {
     return(
         <div className="h-screen flex flex-col justify-center justify-items-center items-center bg-gradient-to-b from-slate-800 to-blue-900">
             <div className="h-1/2 w-4/5 flex flex-col justify-center justify-items-center items-center bg-slate-200/80 rounded-md shadow-lg">
                 <div className="basis-1/4">
-                    <h1 className="text-center lg:text-5xl sm:text-xl md:text-2xl font-bold">Login</h1>
+                    <h1 className="text-center lg:text-5xl sm:text-xl md:text-2xl font-bold">Join us!</h1>
                 </div>
+                <SignUpForm />
             </div>
         </div>
     );
